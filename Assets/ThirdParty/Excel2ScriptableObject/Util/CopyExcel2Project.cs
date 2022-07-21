7b35bf304169d40dc18cd8f1883e44d4a42d7ce8
yangilong <1762608119@qq.com>
2022-07-21 22:57:32 +0800

    添加读取配置表功能

---
+++ Assets/ThirdParty/Excel2ScriptableObject/Util/CopyExcel2Project.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+namespace Excel2Charp
+{
+    public class CopyExcel2Project
+    {
+        public static void CopyExcelToProject()
+        {
+            string originPath = Path.Combine(Application.dataPath, "../../YG_Common/ExcelConfig/");
+            string writePath = Path.Combine(Application.dataPath, "../Excels/");
+            Directory.Delete(writePath, true);
+            Directory.CreateDirectory(writePath);
+            string[] fileStrs = Directory.GetFiles(originPath, "*.xlsx");
+            for (int i = 0; i < fileStrs.Length; i++)
+            {
+                FileInfo fi = new FileInfo(fileStrs[i]);
+                fi.CopyTo(writePath + fi.Name);
+            }
+        }
+
+    }
+}
