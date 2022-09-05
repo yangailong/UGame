a405789645e88c7a2695ec6a20e9f07923d0c1f3
yangailong <1762608119@qq.com>
2022-09-05 18:48:50 +0800

    添加加密功能

---
+++ HotFixAssembly/Code/Core/Cryp/Cryp.cs
@@ -0,0 +1,191 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+namespace UGame_Remove
+{
+    public class Cryp
+    {
+
+        /// <summary>
+        /// 加密字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static string? EncryptStr(string value, string key)
+        {
+            try
+            {
+                Byte[] keyByte = Encoding.UTF8.GetBytes(key);
+                Byte[] encrypt = Encoding.UTF8.GetBytes(value);
+                var aes = Aes.Create();
+                aes.Key = keyByte;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+                ICryptoTransform cTransform = aes.CreateEncryptor();
+
+                Byte[] resultArray = cTransform.TransformFinalBlock(encrypt, 0, encrypt.Length);
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 解密字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static string? DecryptStr(string value, string key)
+        {
+            try
+            {
+                Byte[] keyArray = Encoding.UTF8.GetBytes(key);
+                Byte[] toEncryptArray = Convert.FromBase64String(value);
+                var aes = Aes.Create();
+                aes.Key = keyArray;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+                ICryptoTransform cTransform = aes.CreateDecryptor();
+                Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                return Encoding.UTF8.GetString(resultArray);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex);
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// AES 算法加密(ECB模式) 将明文加密
+        /// </summary>
+        /// <param name="toEncryptArray">明文</param>
+        /// <param name="key">密钥</param>
+        /// <returns>加密后base64编码的密文</returns>
+        public static byte[]? AesEncrypt(byte[] toEncryptArray, string key)
+        {
+            try
+            {
+                byte[] keyArray = Encoding.UTF8.GetBytes(key);
+
+                var aes = Aes.Create();
+                aes.Key = keyArray;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = aes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return resultArray;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// AES 算法解密(ECB模式) 将密文base64解码进行解密，返回明文
+        /// </summary>
+        /// <param name="toDecryptArray">密文</param>
+        /// <param name="key">密钥</param>
+        /// <returns>明文</returns>
+        public static byte[]? AesDecrypt(byte[] toDecryptArray, string key)
+        {
+            try
+            {
+                byte[] keyArray = Encoding.UTF8.GetBytes(key);
+
+                var aes = Aes.Create();
+                aes.Key = keyArray;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = aes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toDecryptArray, 0, toDecryptArray.Length);
+                return resultArray;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// AES 算法加密(ECB模式) 无padding填充，用于分块解密
+        /// </summary>
+        /// <param name="toEncryptArray">明文</param>
+        /// <param name="key">密钥</param>
+        /// <returns>加密后base64编码的密文</returns>
+        public static byte[]? AesEncryptWithNoPadding(byte[] toEncryptArray, string key)
+        {
+            try
+            {
+                byte[] keyArray = Encoding.UTF8.GetBytes(key);
+
+                var aes = Aes.Create();
+                aes.Key = keyArray;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.None;
+
+                ICryptoTransform cTransform = aes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return resultArray;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// AES 算法解密(ECB模式) 无padding填充，用于分块解密
+        /// </summary>
+        /// <param name="toDecryptArray">密文</param>
+        /// <param name="key">密钥</param>
+        /// <returns>明文</returns>
+        public static byte[]? AesDecryptWithNoPadding(byte[] toDecryptArray, string key)
+        {
+            try
+            {
+                byte[] keyArray = Encoding.UTF8.GetBytes(key);
+
+                var aes = Aes.Create();
+                aes.Key = keyArray;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.None;
+
+                ICryptoTransform cTransform = aes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toDecryptArray, 0, toDecryptArray.Length);
+                return resultArray;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                return null;
+            }
+        }
+
+
+    }
+}
+
--- HotFixAssembly/Code/Core/PlayerPrefs/PlayerPrefs.cs
+++ HotFixAssembly/Code/Core/PlayerPrefs/PlayerPrefs.cs
@@ -1,39 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
+using PlayerPrefs = UnityEngine.PlayerPrefs;
 
 namespace UGame_Remove
 {
     public class PlayerPrefs
     {
-        private const string JSaverKeys = "JEngine.Core.JSaver.Keys";
+        private const string SaverKeys = "UGame.Core.Saver.Keys";
 
         private static readonly string defaultEncryptKey = ""; //InitJEngine.Instance.key;
 
+        public static void SaveString(string key,string value)
+        {
+            var s=UnityEngine.PlayerPrefs.GetString(SaverKeys, defaultEncryptKey);
+        }
+
+
         private static void AddJSaverKeys(string key)
         {
-
+            var s = UnityEngine.PlayerPrefs.GetString(SaverKeys, key);
+            if (!s.Split(',').ToList().Contains(key))
+            {
+                var sb = new StringBuilder(s);
+                sb.Append($",{key}");
+                s = sb.ToString();
+            }
+            UnityEngine.PlayerPrefs.SetString(SaverKeys, s);
         }
+
 
         public static string SaveAsString<T>(string dataName, T val, string encryptKey = null)
         {
-            return string.Empty;
+            if (String.IsNullOrEmpty(encryptKey))
+            {
+                encryptKey = defaultEncryptKey;
+            }
+            if (encryptKey.Length != 16)
+            {
+                var ex = new Exception("encryptKey needs to be 16 characters!");
+                Debug.LogError($"[JSaver] 错误：{ex.Message}, {ex.Data["StackTrace"]}");
+                return null;
+            }
+            string strData = val.ToString();
+            var result = CryptoMgr.EncryptStr(strData, encryptKey);
+
+            UnityEngine.PlayerPrefs.SetString(dataName, result);
+            AddJSaverKeys(dataName);
+            return result;
         }
-
-
-        public static byte[] SaveAsProtobufBytes<T>(string dataName, T val, string encryptKey = null) where T : class
-        {
-
-            return null;
-        }
-
-
-        public static string SaveAsJSON<T>(string dataName, T val, string encryptKey = null)
-        {
-            return string.Empty;
-        }
-
     }
 }
