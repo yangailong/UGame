959487bf80b88de289230472032da13d7a3416d9
yangilong <1762608119@qq.com>
2022-09-19 22:59:58 +0800

    功能完善

--- HotFixAssembly/Scripts/Core/Audio/Audio/Audio3DPlayer.cs
+++ HotFixAssembly/Scripts/Core/Audio/Audio/Audio3DPlayer.cs
@@ -32,7 +32,29 @@ namespace UGame_Remove
             }
         }
 
+        public AudioAsset PlayMusic(GameObject owner, AudioClip audioClip, int channel = 0, bool isLoop = true, float volumeScale = 1, float delay = 0f)
+        {
+            AudioAsset audioAsset = null;
 
+            if (owner == null)
+            {
+                throw new ArgumentException($"cannot play 3d audioClip,owner is null");
+            }
+
+            if (!bgMusicDic.TryGetValue(owner, out var pairs))
+            {
+                pairs = new Dictionary<int, AudioAsset>();
+                bgMusicDic.Add(owner, pairs);
+            }
+
+            if (pairs.TryGetValue(channel, out audioAsset))
+            {
+
+            }
+
+
+            return audioAsset;
+        }
 
     }
 }
