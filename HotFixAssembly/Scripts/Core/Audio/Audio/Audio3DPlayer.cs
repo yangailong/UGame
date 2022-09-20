2dd206611d0fc59b64207b641ee9509f83647634
yangailong <1762608119@qq.com>
2022-09-20 19:51:46 +0800

    音效功能完善

--- HotFixAssembly/Scripts/Core/Audio/Audio/Audio3DPlayer.cs
+++ HotFixAssembly/Scripts/Core/Audio/Audio/Audio3DPlayer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UGame_Remove
@@ -19,7 +21,6 @@ namespace UGame_Remove
         }
 
 
-
         public void SetMusicVolume(float volume)
         {
             base.MusicVolume = volume;
@@ -32,7 +33,22 @@ namespace UGame_Remove
             }
         }
 
-        public AudioAsset PlayMusic(GameObject owner, AudioClip audioClip, int channel = 0, bool isLoop = true, float volumeScale = 1, float delay = 0f)
+
+        public void SetSFXVolume(float volume)
+        {
+            base.SFXVolume = volume;
+
+            foreach (var item in sfxDic.Values)
+            {
+                foreach (var cell in item)
+                {
+                    cell.TotleVolume = volume;
+                }
+            }
+        }
+
+
+        public AudioAsset Play(GameObject owner, AudioClip audioClip, int channel = 0, bool isLoop = true, float volumeScale = 1, float delay = 0f)
         {
             AudioAsset audioAsset = null;
 
@@ -47,13 +63,253 @@ namespace UGame_Remove
                 bgMusicDic.Add(owner, pairs);
             }
 
-            if (pairs.TryGetValue(channel, out audioAsset))
+            if (!pairs.TryGetValue(channel, out audioAsset))
             {
+                audioAsset = base.CreateAudioAsset(owner, true, AudioSourceType.Music);
+                pairs.Add(channel, audioAsset);
+            }
 
+            base.Play(audioAsset, audioClip, isLoop, volumeScale, delay);
+
+            return audioAsset;
+        }
+
+
+        public void Pause(GameObject owner, int channel, bool isPause)
+        {
+            if (owner == null)
+            {
+                throw new ArgumentException($"cannot play 3d audioClip,owner is null");
+            }
+
+            if (bgMusicDic.TryGetValue(owner, out var pairs))
+            {
+                if (pairs.TryGetValue(channel, out var audioAsset))
+                {
+                    base.Pause(audioAsset);
+                }
+            }
+        }
+
+
+        public void PauseAll(bool isPause)
+        {
+            foreach (var item in bgMusicDic.Keys)
+            {
+                foreach (var cell in bgMusicDic[item].Keys)
+                {
+                    Pause(item, cell, isPause);
+                }
+            }
+        }
+
+
+        public void Stop(GameObject owner, int channel)
+        {
+            if (bgMusicDic.TryGetValue(owner, out var pairs))
+            {
+                if (pairs.TryGetValue(channel, out var audioAsset))
+                {
+                    base.Stop(audioAsset);
+                }
+            }
+        }
+
+
+        public void StopSubAll(GameObject owner)
+        {
+            if (bgMusicDic.TryGetValue(owner, out var pairs))
+            {
+                foreach (var cell in pairs.Keys)
+                {
+                    Stop(owner, cell);
+                }
+            }
+        }
+
+
+        public void StopAll()
+        {
+            foreach (var item in bgMusicDic.Keys)
+            {
+                StopSubAll(item);
+            }
+        }
+
+
+        public void Destroy(GameObject owner)
+        {
+            if (bgMusicDic.TryGetValue(owner, out var pairs))
+            {
+                StopSubAll(owner);
+
+                foreach (var cell in pairs.Values)
+                {
+                    GameObject.Destroy(cell.audioSource);
+                }
+            }
+
+            bgMusicDic.Remove(owner);
+        }
+
+
+        public void DestroyAll()
+        {
+            foreach (var item in bgMusicDic.Keys)
+            {
+                Destroy(item);
+            }
+
+            bgMusicDic.Clear();
+        }
+
+
+        public void PlaySFX(GameObject owner, AudioClip audioClip, float volumeScale = 1f, float delay = 0f)
+        {
+            AudioAsset audioAsset = base.CreateAudioAsset(owner, true, AudioSourceType.SFX);
+
+            if (!sfxDic.TryGetValue(owner, out var audioAssets))
+            {
+                audioAssets = new List<AudioAsset>() { audioAsset };
+                sfxDic.Add(owner, audioAssets);
+            }
+
+            base.Play(audioAsset, audioClip, false, volumeScale, delay);
+
+            ClearSFXSubDirtyAudioAsset(owner);
+        }
+
+
+        public void PauseSFXAll(bool isPause)
+        {
+            foreach (var item in sfxDic.Values)
+            {
+                foreach (var cell in item)
+                {
+                    if (isPause)
+                    {
+                        cell.Pause();
+                    }
+                    else
+                    {
+                        cell.Play();
+                    }
+                }
+            }
+        }
+
+
+        public void DestroySFX(GameObject owner)
+        {
+            if (sfxDic.TryGetValue(owner, out var audioAssets))
+            {
+                foreach (var item in audioAssets)
+                {
+                    GameObject.Destroy(item.audioSource);
+                }
+
+                audioAssets.Clear();
+
+                sfxDic.Remove(owner);
+            }
+        }
+
+
+        public void DestroySFXAll()
+        {
+            foreach (var item in sfxDic.Keys)
+            {
+                DestroySFX(item);
+            }
+
+            sfxDic.Clear();
+        }
+
+
+        private void ClearSFXSubDirtyAudioAsset(GameObject owner)
+        {
+            List<AudioAsset> dirty = new List<AudioAsset>();
+
+            if (sfxDic.ContainsKey(owner))
+            {
+                var arr = sfxDic[owner];
+
+                foreach (var item in arr)
+                {
+                    item.CheckState();
+
+                    if (item.PlayState == AudioPlayState.Stop)
+                    {
+                        dirty.Add(item);
+                    }
+                }
+
+                foreach (var item in dirty)
+                {
+                    GameObject.Destroy(item.audioSource);
+                    base.DestroyAudioAsset(item);
+                    arr.Remove(item);
+                }
+
+                dirty.Clear();
+            }
+        }
+
+
+        public void ClearDirtyAudioAsset()
+        {
+            var dirty = new List<GameObject>();
+            dirty.AddRange(bgMusicDic.Keys);
+
+            foreach (var item in dirty)
+            {
+                if (item == null)
+                {
+                    bgMusicDic.Remove(item);
+                }
             }
 
 
-            return audioAsset;
+            foreach (var dic in bgMusicDic)
+            {
+                foreach (var item in dic.Value)
+                {
+                    if (item.Value.PlayState == AudioPlayState.Stop)
+                    {
+                        base.DestroyAudioAsset(bgMusicDic[dic.Key][item.Key]);
+                        bgMusicDic[dic.Key].Remove(item.Key);
+
+                        continue;
+                    }
+                }
+            }
+
+
+            if (sfxDic.Count > 0)
+            {
+                dirty.Clear();
+                dirty.AddRange(sfxDic.Keys);
+                for (int i = 0; i < dirty.Count; i++)
+                {
+                    if (dirty[i] == null)
+                    {
+                        sfxDic.Remove(dirty[i]);
+                    }
+                }
+                foreach (var list in sfxDic)
+                {
+                    foreach (var item in list.Value)
+                    {
+                        item.CheckState();
+                        if (item.PlayState == AudioPlayState.Stop)
+                        {
+                            base.DestroyAudioAsset(item);
+                            sfxDic[list.Key].Remove(item);
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
     }
--- HotFixAssembly/Scripts/Core/Audio/Audio/AudioPlayManager.cs
+++ HotFixAssembly/Scripts/Core/Audio/Audio/AudioPlayManager.cs
@@ -10,6 +10,87 @@ namespace UGame_Remove
         public static Audio3DPlayer a3DPlayer = null;
 
 
+        public static void Init()
+        {
+            GameObject go = new GameObject("[AudioManager]");
+            DontDestroyOnLoad(go);
+
+            a2DPlayer = new Audio2DPlayer(go.AddComponent<AudioPlayManager>());
+            a3DPlayer = new Audio3DPlayer(go.AddComponent<AudioPlayManager>());
+
+
+            TotleVolume = float.Parse(PrefsManager.GetString("TotleVolume", "1"));
+            MusicVolume = float.Parse(PrefsManager.GetString("MusicVolume", "1"));
+            SFXVolume = float.Parse(PrefsManager.GetString("SFXVolume", "1"));
+        }
+
+
+        private static float totleVolume = 1f;
+        public static float TotleVolume
+        {
+            get { return totleVolume; }
+            set
+            {
+                totleVolume = Mathf.Clamp01(value);
+                // SetMusicVolume();
+                //SetSFXVolume();
+
+            }
+        }
+
+        private static float musicVolume = 1f;
+
+
+        public static float MusicVolume
+        {
+            get
+            {
+                return musicVolume;
+            }
+
+            set
+            {
+                musicVolume = Mathf.Clamp01(value);
+                //SetMusicVolume();
+            }
+        }
+
+        private static float sfxVolume = 1f;
+        public static float SFXVolume
+        {
+            get
+            {
+                return sfxVolume;
+            }
+
+            set
+            {
+                sfxVolume = Mathf.Clamp01(value);
+                //SetSFXVolume();
+            }
+        }
+
+
+        private static void SetMusicVolume()
+        {
+            a2DPlayer.SetMusicVolume(totleVolume * musicVolume);
+            a3DPlayer.SetMusicVolume(totleVolume * musicVolume);
+        }
+
+        private static void SetSFXVolume()
+        {
+            a2DPlayer.SetSFXVolume(totleVolume * sfxVolume);
+            a3DPlayer.SetSFXVolume(totleVolume * sfxVolume);
+        }
+
+        public static void SaveVolume()
+        {
+            PrefsManager.SetString("TotleVolume", $"{TotleVolume}");
+            PrefsManager.SetString("MusicVolume", $"{MusicVolume}");
+            PrefsManager.SetString("SFXVolume", $"{SFXVolume}");
+        }
+
+
 
     }
 }
