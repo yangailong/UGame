260c32f17995db3af544b37c2122a653f15f0c53
yangailong <1762608119@qq.com>
2022-10-22 17:09:57 +0800

    添加 WebSocket4Net.dll 以及WebSocket4Net的依赖包SuperSocket.ClientEngine.dll

---
+++ HotFixAssembly/Scripts/Core/Network/WebSocket/WebSocketEvent/WebSocketEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf;
+using System;
+using UnityEngine;
+
+namespace UGame_Remove
+{
+
+    public interface IWebSocketEvent
+    {
+        void Dispatch(byte[] receiveBuffer, int startPos);
+    }
+
+
+    public class WebSocketEventArgs<T> : IWebSocketEvent where T : IMessage, new()
+    {
+        private int id;
+
+        private Action<int, T> callback;
+
+
+        public WebSocketEventArgs(int id, Action<int, T> callback)
+        {
+            this.id = id;
+            this.callback = callback;
+        }
+
+
+        public void Dispatch(byte[] receiveBuffer, int startPos)
+        {
+            //var msg = new T().Descriptor.Parser.ParseFrom(receiveBuffer, startPos, receiveBuffer.Length - startPos);
+
+            //Debug.Log($"recv msgId: {id}, {JsonUtility.ToJson(msg)}");
+
+          
+
+            //callback?.Invoke(id, (T)msg);
+        }
+
+    }
+
+}
+
