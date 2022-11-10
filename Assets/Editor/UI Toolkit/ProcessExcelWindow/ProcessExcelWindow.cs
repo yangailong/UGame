7a62d80802d31510a7a0fa106b3de80f64186913
yangailong <1762608119@qq.com>
2022-11-10 21:02:05 +0800

    添加代码注释

--- Assets/Editor/UI Toolkit/ProcessExcelWindow/ProcessExcelWindow.cs
+++ Assets/Editor/UI Toolkit/ProcessExcelWindow/ProcessExcelWindow.cs
@@ -2,27 +2,42 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using System.ComponentModel;
+using System.Drawing;
 
-
-public class ProcessExcelWindow : EditorWindow
+namespace UGame_Local_Editor
 {
-    [MenuItem("Tools/UGame/Excel表数据转ScriptableObject %E")]
-    public static void ShowExample()
+    public class ProcessExcelWindow : EditorWindow
     {
-        ProcessExcelWindow wnd = GetWindow<ProcessExcelWindow>();
-        wnd.titleContent = new GUIContent("ProcessExcelWindow");
-    }
-
-    public void CreateGUI()
-    {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
-
-        // Import UXML
-        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Toolkit/ProcessExcelWindow/ProcessExcelWindow.uxml");
-        VisualElement labelFromUXML = visualTree.Instantiate();
-        root.Add(labelFromUXML);
+        [MenuItem("Tools/UGame/Excel表数据转ScriptableObject %E")]
+        public static void ShowExample()
+        {
+            ProcessExcelWindow wnd = GetWindow<ProcessExcelWindow>();
+            wnd.titleContent = new GUIContent("ProcessExcelWindow");
+        }
 
 
+        private ProcessExcelImpl impl = null;
+
+        public void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // Import UXML
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UI Toolkit/ProcessExcelWindow/ProcessExcelWindow.uxml");
+            VisualElement labelFromUXML = visualTree.Instantiate();
+            root.Add(labelFromUXML);
+
+            impl = new ProcessExcelImpl();
+
+            TextElement textElement = new Button() { text = "C# Button" };
+            textElement.name = "Button";
+            textElement.text = "CfgLanager";
+            //textElement  ProcessExcelImpl
+            root.Q<ScrollView>().Add(textElement);
+
+
+        }
     }
 }
