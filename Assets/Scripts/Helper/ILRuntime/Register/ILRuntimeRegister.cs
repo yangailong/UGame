7fc1bbac1222c70afb2ddfabe3c6314715665c69
yangilong <1762608119@qq.com>
2022-12-02 17:25:00 +0800

    临时删除

--- Assets/Scripts/Helper/ILRuntime/Register/ILRuntimeRegister.cs
+++
@@ -1,9 +0,0 @@
-﻿using AppDomain = ILRuntime.Runtime.Enviorment.AppDomain;
-namespace UGame_Local
-{
-    public interface ILRuntimeRegister
-    {
-        void Register(AppDomain appdomain);
-    }
-
-}
